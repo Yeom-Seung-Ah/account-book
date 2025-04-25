07705f0ec508e22ef4baf20e331b47f478d34dbe
Yeom-Seung-Ah <ysa2780@naver.com>
2025-04-25 19:27:34 +0900

    Summary 기능 완성

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import css from "./../css/Summary.module.css";
+import useTransactions from "../../hooks/useTransactions";
 
 const Summary = () => {
-  return <div className={`${css.container} summary`}>요약 영역 입니다.</div>;
+  const { transactions } = useTransactions();
+
+  const income = transactions
+    .filter((t) => t.type === "income")
+    .reduce((acc, curr) => acc + curr.amount, 0);
+
+  const expense = transactions
+    .filter((t) => t.type === "expense")
+    .reduce((acc, curr) => acc + curr.amount, 0);
+
+  const balance = income - expense;
+
+  return (
+    <div className={`${css.summaryContainer} summary`}>
+      <div className={css.balanceBox}>
+        <p>잔액</p>
+        <h3>₩{balance.toLocaleString()}</h3>
+      </div>
+      <div className={css.incomeExpenseBox}>
+        <div className={css.incomeBox}>
+          <p>수입</p>
+          <span className={css.income}>₩{income.toLocaleString()}</span>
+        </div>
+        <div className={css.separator}></div>
+        <div className={css.expenseBox}>
+          <p>지출</p>
+          <span className={css.expense}>₩{expense.toLocaleString()}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
